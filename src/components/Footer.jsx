9eb90873129bcc16fd872a10e84fca9727549f5c
FrancaTM <francatm@outlook.com>
2024-02-26 17:20:55 -0300

    comment-based prompting add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// An exported Footer component that displays my name "Tulio M. Franca" and the copyright sign.
+// The component should be imported and rendered in the App component.
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>
+                <small>
+                    &copy; 2024 Tulio M. Franca
+                </small>
+            </p>
+        </footer>
+    );
+}
