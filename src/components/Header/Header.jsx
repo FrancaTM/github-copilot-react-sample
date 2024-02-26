d2d2f361b28e0b8967fd6c720a50c5a68b369504
FrancaTM <francatm@outlook.com>
2024-02-26 17:06:00 -0300

    trigger completion via variable name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,14 @@ import './Header.css';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 export default function Header() {
+  const firstDescription = reactDescriptions[0];
+
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        React concepts you will need for almost any app you are going to build!
+        {firstDescription} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
